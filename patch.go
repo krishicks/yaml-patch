351e0ba6d34aaf67ba3ab4a63fa5adad878ba500
Chris Suran <suran.c@gmail.com>
2018-10-11 01:40:27 -0400

    Switching to Decode from Unmarshal to support multiple documents in the input

--- patch.go
+++ patch.go
@@ -2,6 +2,7 @@ package yamlpatch
 
 import (
 	"fmt"
+	"bytes"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -23,35 +24,44 @@ func DecodePatch(bs []byte) (Patch, error) {
 
 // Apply returns a YAML document that has been mutated per the patch
 func (p Patch) Apply(doc []byte) ([]byte, error) {
-	var iface interface{}
-	err := yaml.Unmarshal(doc, &iface)
-	if err != nil {
-		return nil, fmt.Errorf("failed unmarshaling doc: %s\n\n%s", string(doc), err)
-	}
-
 	var c Container
-	c = NewNode(&iface).Container()
+	docdec := yaml.NewDecoder(bytes.NewReader(doc))
+    for {
+		var iface interface{}
 
-	for _, op := range p {
-		pathfinder := NewPathFinder(c)
-		if op.Path.ContainsExtendedSyntax() {
-			paths := pathfinder.Find(string(op.Path))
-			if paths == nil {
-				return nil, fmt.Errorf("could not expand pointer: %s", op.Path)
-			}
+		err := docdec.Decode(&iface)
+		if err != nil {
+			return nil, fmt.Errorf("failed unmarshaling doc: %s\n\n%s", string(doc), err)
+		}
 
-			for _, path := range paths {
-				newOp := op
-				newOp.Path = OpPath(path)
-				err = newOp.Perform(c)
+		// Check for no more documents
+		if iface == nil {
+        	break
+        }
+
+		c = NewNode(&iface).Container()
+
+		for _, op := range p {
+			pathfinder := NewPathFinder(c)
+			if op.Path.ContainsExtendedSyntax() {
+				paths := pathfinder.Find(string(op.Path))
+				if paths == nil {
+					return nil, fmt.Errorf("could not expand pointer: %s", op.Path)
+				}
+
+				for _, path := range paths {
+					newOp := op
+					newOp.Path = OpPath(path)
+					err = newOp.Perform(c)
+					if err != nil {
+						return nil, err
+					}
+				}
+			} else {
+				err = op.Perform(c)
 				if err != nil {
 					return nil, err
 				}
-			}
-		} else {
-			err = op.Perform(c)
-			if err != nil {
-				return nil, err
 			}
 		}
 	}
